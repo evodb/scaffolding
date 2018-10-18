7565e6b9a92533e7ee91f2b7eb9ed9274d371fe8
ynfeng <fynwin@gmail.com>
2018-10-18 12:53:02 +0800

    修改单元测试

--- idaccess/src/test/java/io/evodb/idaccess/domain/model/identity/TenantJpaRepositoryTest.java
+++ idaccess/src/test/java/io/evodb/idaccess/domain/model/identity/TenantJpaRepositoryTest.java
@@ -24,10 +24,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 @DataJpaTest
 @Import(JpaTenantRepository.class)
 public class TenantJpaRepositoryTest {
